24896b248a5b710150ce462f2569cf60981e640b
aswingururaj <30689501+aswingururaj@users.noreply.github.com>
2019-03-03 19:58:31 +0530

    Add files via upload

---
+++ sketch_mar03a.ino
@@ -0,0 +1,113 @@
+#include <Wire.h>
+#include <Servo.h>
+#define SerialPort Serial
+#include <SparkFunMPU9250-DMP.h>
+MPU9250_DMP imu;
+double roll , pitch, yaw;
+long int pre_ts=0;
+int a[10]={0};
+int i,j=0,k;
+float sum=0.0,average=0.0;
+void setup() 
+{
+
+    {
+  SerialPort.begin(115200);
+
+ if (imu.begin() != INV_SUCCESS)
+  {
+    while (1)
+    {
+      SerialPort.println("Unable to communicate with MPU-9250");
+      SerialPort.println("Check connections, and try again.");
+      SerialPort.println();
+      delay(3000);
+    }
+  }
+
+ 
+  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+
+ 
+  imu.setGyroFSR(250); // Set gyro to 2000 dps
+  // Accel options are +/- 2, 4, 8, or 16 g
+  imu.setAccelFSR(2); // Set accel to +/-2g
+  imu.setLPF(10); // Set LPF corner frequency to 5Hz
+  imu.setSampleRate(10); // Set sample rate to 10Hz
+  imu.setCompassSampleRate(50); // Set mag rate to 10Hz
+}
+
+  pre_ts=millis();
+}
+
+//}
+void loop() 
+{
+    
+    if ( imu.dataReady() )
+  {
+    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+    printIMUData(millis()-pre_ts);
+    pre_ts=millis();
+  }
+}
+
+void printIMUData(long int dt)
+{  
+  
+  float accelX = imu.calcAccel(imu.ax);
+  float accelY = imu.calcAccel(imu.ay);
+  float accelZ = imu.calcAccel(imu.az);
+  float gyroX = imu.calcGyro(imu.gx)/57.3;
+  float gyroY = imu.calcGyro(imu.gy)/57.3;
+  float gyroZ = imu.calcGyro(imu.gz)/57.3;
+  float magX = imu.calcMag(imu.mx);
+  float magY = imu.calcMag(imu.my);
+  float magZ = imu.calcMag(imu.mz);
+
+  //SerialPort.println("Accel: " + String(accelX) + ", " + String(accelY) + ", " + String(accelZ) + " g");
+  //SerialPort.println("Gyro: " + String(gyroX) + ", " + String(gyroY) + ", " + String(gyroZ) + " dps");
+ //SerialPort.println("Mag: " + String(magX) + ", " + String(magY) + ", " + String(magZ) + " uT");
+  //SerialPort.println("Time: " + String(imu.time) + " ms");
+
+//Euler angle from accel
+
+ 
+   pitch = atan2 (accelY ,( sqrt ((accelX * accelX) + (accelZ * accelZ))));
+   roll = atan2(-accelX ,( sqrt((accelY * accelY) + (accelZ * accelZ))));
+
+   // yaw from mag
+
+   float Yh = (magY * cos(roll)) - (magZ * sin(roll));
+   float Xh = (magX * cos(pitch))+(magY * sin(roll)*sin(pitch)) + (magZ * cos(roll) * sin(pitch));
+
+   yaw =  atan2(Yh, Xh);
+
+
+    roll = roll*57.3;
+    pitch = pitch*57.3;
+    yaw = yaw*57.3; 
+   move();
+   j++;
+   a[0]=yaw;
+   if(j>=10)
+   {
+    for(k=0;k<10;k++)
+    sum+=a[k];
+    average=sum/10;
+    yaw=average;
+    sum=0;
+   }
+   //Serial.println("pitch"  + String( pitch) );
+   //Serial.println("roll" + String( roll));
+   Serial.println("yaw" + String( yaw ));
+
+
+}
+
+
+void move()
+{
+  for(i=8;i>=0;i--)
+   a[i+1]=a[i];
+}
